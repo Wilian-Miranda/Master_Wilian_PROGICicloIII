3c0a6edb5c8bdebc93f287b590a0c76e06a3b9a6
Wilian-Miranda <69541095+Wilian-Miranda@users.noreply.github.com>
2021-02-16 16:53:08 -0600

    CARPETA DE HISTORIALES DE EJERCICIOS

---
+++ Historial de ejercicios/LOGIN/WilianMiranda01/WilianMiranda01/VISTA/FmrHome.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WilianMiranda01.VISTA
+{
+    public partial class FmrHome : Form
+    {
+        public FmrHome()
+        {
+            InitializeComponent();
+        }
+
+        private void clickParaMásInformaciónToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FmrAcercaDe infoUs = new FmrAcercaDe();
+            //el formulario de abre sobre el formulario padre
+            infoUs.MdiParent = this;
+            infoUs.Show();
+        }
+    }
+}
